7459bea3c02a7a8dcbe2beec46c331db7b578e9a
baptiste2002 <43027910+baptiste2002@users.noreply.github.com>
2018-10-03 15:49:40 +0200

    oefening avogadro

--- 04-Variabelen/Avogadro.py
+++ 04-Variabelen/Avogadro.py
@@ -1,11 +1,11 @@
 het_aantal_deeljes_s = float(input('Geef het aantal deeltjes: '))
 
 M = 32.06
-Na = 1 / (6.020 * 10 ** 23)
+Na = 6.020 * 10 ** 23
 
 m = M * het_aantal_deeljes_s
 
-N = het_aantal_deeljes_s / Na
+N = het_aantal_deeljes_s * Na
 
 resultaat_1 = m
 resultaat_2 = N
